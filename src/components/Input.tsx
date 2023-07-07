94d4ebc18721841904f968f041d5102ac8341689
yassine krika <yassinekrika2016@gmail.com>
2023-07-07 11:17:16 +0100

    Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,18 @@
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <form action="" className="flex flex-col gap-4">
+      <div className="flex flex-col">
+        <label className="text-white">Enter your net task:</label>
+        <input type="text" className="p-1 rounded-sm" />
+      </div>
+      <button
+        type="button"
+        className="bg-green100 rounded-lg hover:bg-green-200 [-1"
+      >
+        Add task
+      </button>
+    </form>
   )
 }
 
