dc15cfe474bacdca1cf58cb563119e6fa1ea975d
yassine krika <yassinekrika2016@gmail.com>
2023-07-07 10:54:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Container from "./components/Container"
+import Input from "./components/Input"
+import Summary from "./components/Summary"
+import Tasks from "./components/Tasks"
+
+const App = () => {
+  return (
+    <div className='flex justify-center m5'>
+
+    </div>
+  )
+}
+
+export default App
